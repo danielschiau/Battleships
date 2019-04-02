7a6eb554ca5f57fea19c56c838d8fdf7875d05c0
Daniel <daniel.i.schiau@gmail.com>
2019-04-02 17:29:28 +0300

    BS-2 Add ViewModel navigation

---
+++ Battleships.Presenter/Navigation/INavigationService.cs
@@ -0,0 +1,10 @@
+﻿using Battleships.Presenter.Pages.Base;
+
+namespace Battleships.Presenter.Navigation
+{
+    public interface INavigationService
+    {
+        void NavigateToViewModel(BaseViewModel viewModel);
+        void PopUpMessage(string title, string message);
+    }
+}
